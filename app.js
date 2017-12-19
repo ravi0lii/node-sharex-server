d42a11ac6fa7e26b00dcf11651e30e254f1ed41e
Moquo <mail@moquo.de>
2017-12-19 18:48:29 +0100

    npm install body-parser

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ var logger = require('./logger.js');
 // Express basic stuff
 var express = require('express');
 var app = express();
+// body-parser middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Start web server
 app.listen(PORT, function() {
