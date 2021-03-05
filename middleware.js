2155f847dbe2dd4ce365697d3fd2f9929b2a5e60
SummitStudiosDev <67283504+SummitStudiosDev@users.noreply.github.com>
2021-03-04 20:27:56 -0800

    fixed some problems
    fixed
    1. uploading
    (will now show deletion key correctly and work)
    2. deleting
    (can now delete, previously would not accept the key in the url param as an auth key, would only check the key in form)

--- middleware.js
+++ middleware.js
@@ -18,16 +18,23 @@ module.exports.keyRequired = function (req, res, next) {
 
     // Check if key is registered
     var key = req.body.key;
-    if (keys.indexOf(key) == -1) {
-        logger.auth('Failed authentication with key ' + key);
+	var key2 = req.query.key;
+    if (keys.indexOf(key) == -1 && keys.indexOf(key2) == -1 ) {
+        logger.auth('Failed authentication with key ' + key +' and '+key2);
         response.invalidKey(res);
         return;
     }
 
     // Add short key ot request locals
-    req.locals = {
-        shortKey: key.substr(0, 3) + '...'
-    };
+	if (typeof key !== 'undefined'){
+		req.locals = {
+			shortKey: key.substr(0, 3) + '...'
+		};
+	}else if(typeof key2 !== 'undefined'){
+		req.locals = {
+			shortKey: key2.substr(0, 3) + '...'
+		};
+	}
 
     logger.auth('Authentication with key ' + req.locals.shortKey + ' succeeded');
 
